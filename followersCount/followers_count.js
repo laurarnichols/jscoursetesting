75f27986fd5be33f0f01c448ba24abd02253ded9
Laura Nichols <laurarnichols5@gmail.com>
2024-12-16 22:57:22 -0500

    Add files from followers count lab

---
+++ followersCount/followers_count.js
@@ -0,0 +1,25 @@
+let count = 0;
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML = count; 
+}
+
+function checkCountValue() {
+  if (count === 10) {
+    alert("Your Instagram post gained 10 followers! Congratulations!");
+  } else if (count === 20) {
+    alert("Your Instagram post gained 20 followers! Keep it up!");
+  }
+}
+
+function increaseCount() {
+    count++; 
+    displayCount(); 
+    checkCountValue();
+}
+
+function resetCount() {
+    count = 0;
+    displayCount(); 
+    alert("Followers count has been reset.");
+}
